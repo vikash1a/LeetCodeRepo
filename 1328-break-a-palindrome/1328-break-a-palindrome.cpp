0aee17d99b8afee97712fa57a7aaf10013d138b8
Vikash Sinha <vikash1a2b3c@gmail.com>
2022-10-10 09:10:07 -0700

    Time: 0 ms (100.00%), Space: 6.3 MB (45.53%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        int ns = palindrome.size()-1;
+        if(ns+1 == 1)return "";
+        
+        for(int i=0;i<=ns;i++){
+            if((ns+1)%2==1 && i==(ns+1)/2)continue;
+            if(palindrome[i]>'a'){
+                // cout<<palindrome[i]<<endl;
+                palindrome[i]='a';
+                // cout<<palindrome[i]<<endl;
+                return palindrome;
+            }
+            
+        }
+        palindrome[ns]+=1;
+        return palindrome;
+    }
+};

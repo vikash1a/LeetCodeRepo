bbbd8c9a1adfbbb47a09c52f2b022f654af946e2
Vikash Sinha <vikash1a2b3c@gmail.com>
2022-10-18 07:39:01 -0700

    Time: 32 ms (66.79%), Space: 16.6 MB (80.21%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
+    // int jump(vector<int>& nums) {
+    //     int nn = nums.size()-1;
+    //     if(nn+1 == 1)return 0;
+    //     vector<int> jumpCount (nn+1,1e6);
+    //     jumpCount[0]=0,jumpCount[1]=1;
+    //     for(int i=0;i<=nn;i++){
+    //         for(int j=i+1;j<=i+nums[i] && j<=nn;j++){
+    //             jumpCount[j] = min(jumpCount[j], jumpCount[i]+1);
+    //             // cout<<i<<" "<<j<<" "<<jumpCount[j]<<endl;
+    //         }
+    //     }
+    //     return jumpCount[nn];
+    // }
     int jump(vector<int>& nums) {
         int nn = nums.size()-1;
         if(nn+1 == 1)return 0;
-        vector<int> jumpCount (nn+1,1e6);
-        jumpCount[0]=0,jumpCount[1]=1;
-        for(int i=0;i<=nn;i++){
-            for(int j=i+1;j<=i+nums[i] && j<=nn;j++){
-                jumpCount[j] = min(jumpCount[j], jumpCount[i]+1);
-                // cout<<i<<" "<<j<<" "<<jumpCount[j]<<endl;
+        int i = 0, maxReachable = 0, lastJumpPos = 0, jump =0;
+        while(lastJumpPos < nn){
+            maxReachable = max(maxReachable, i+nums[i]);
+            if(i==lastJumpPos){
+                jump++;
+                lastJumpPos = maxReachable;
             }
+            i++;
         }
-        return jumpCount[nn];
+        return jump;
     }
 };
 /*

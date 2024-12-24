d641bdcef0262e2f7975438cccc4888c86314220
Vikash Sinha <vikash1a2b3c@gmail.com>
2024-12-24 21:25:41 +0530

    Time: 2742 ms (5.10%) | Memory: 8.4 MB (85.20%) - LeetSync

--- 2883-partition-string-into-minimum-beautiful-substrings/partition-string-into-minimum-beautiful-substrings.cpp
+++ 2883-partition-string-into-minimum-beautiful-substrings/partition-string-into-minimum-beautiful-substrings.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
 
-    unordered_map<string,int> unmp;
-    bool isDivisibleBy5(string& s){
+    bool isDivisibleBy5(string& s,int is, int ie ){
         long long num = 0;
-        for(int i = 0;i<=(int)s.size()-1;i++){
-            num+= (s[i]-'0')*((long long)(pow(2,(int)s.size()-1-i)));
+        for(int i = is;i<=ie;i++){
+            num+= (s[i]-'0')*((long long)(pow(2,ie-i)));
         }
         if(num==1)return true;
         while(num>1){
@@ -14,25 +13,26 @@ public:
         }
         return true;
     }
-    int minimumBeautifulSubstrings(string s) {
-        if(unmp.find(s)!=unmp.end())return unmp[s];
+    int util(string &s, int is, int ie){
+        // cout<<is<<" "<<ie<<endl;
         int minValue = INT_MAX;
-        if(s[0]=='0')minValue = -1;
-        else if(s=="1")minValue = 1;
-        else if(isDivisibleBy5(s))minValue =  1;
+        if(s[is]=='0')minValue = -1;
+        else if(s[is]=='1' && (is==ie))minValue = 1;
+        else if(isDivisibleBy5(s, is, ie))minValue =  1;
         else{
-            int ns = s.size()-1;
-            for(int i=1;i<=ns;i++){
-                int m1 = minimumBeautifulSubstrings(s.substr(0,i));
-                int m2 = minimumBeautifulSubstrings(s.substr(i, ns+1));
+            for(int i=is;i<=ie-1;i++){
+                int m1 = util(s,is,i);
+                int m2 = util(s,i+1,ie);
                 // cout<<s1<<"="<<m1<<"  "<<s2<<"="<<m2<<endl;
                 if(m1!=-1 && m2!=-1)minValue = min(minValue, m1+m2);
             }
         }
         if(minValue == INT_MAX)minValue = -1;
-        unmp[s] = minValue;
-        // cout<<s<<" "<<minValue<<endl;
+        // cout<<s.substr(is,ie-is+1)<<" "<<minValue<<endl;
         return minValue;
+    }
+    int minimumBeautifulSubstrings(string s) {
+        return util(s,0,s.size()-1);
     }
 };
 /*

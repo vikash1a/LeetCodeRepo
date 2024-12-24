1736db093fb4bae6d1f163a212bd2b531ee1d91c
Vikash Sinha <vikash1a2b3c@gmail.com>
2024-12-24 20:46:24 +0530

    Time: 1262 ms (5.10%) | Memory: 14.9 MB (6.12%) - LeetSync

---
+++ 2883-partition-string-into-minimum-beautiful-substrings/partition-string-into-minimum-beautiful-substrings.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    // void printBinary(int k){
+    //     while(k>0){
+    //         cout<<(k&1);
+    //         k = k>>1;
+    //     }
+    //     cout<<endl;
+    // }
+    unordered_map<string,int> unmp;
+    bool isDivisibleBy5(string s){
+        long long num = 0;
+        for(int i = 0;i<=(int)s.size()-1;i++){
+            num+= (s[i]-'0')*((long long)(pow(2,(int)s.size()-1-i)));
+        }
+        if(num==1)return true;
+        while(num>1){
+            if(num%5!=0)return false;
+            num/=5;
+        }
+        return true;
+    }
+    int minimumBeautifulSubstrings(string s) {
+        if(unmp.find(s)!=unmp.end())return unmp[s];
+        int minValue = INT_MAX;
+        if(s[0]=='0')minValue = -1;
+        else if(s=="1")minValue = 1;
+        else if(isDivisibleBy5(s))minValue =  1;
+        else{
+            int ns = s.size()-1;
+            for(int i=1;i<=ns;i++){
+                string s1 = s.substr(0,i);
+                string s2 = s.substr(i, ns+1);
+                int m1 = minimumBeautifulSubstrings(s1);
+                int m2 = minimumBeautifulSubstrings(s2);
+                cout<<s1<<"="<<m1<<"  "<<s2<<"="<<m2<<endl;
+                if(m1!=-1 && m2!=-1)minValue = min(minValue, m1+m2);
+            }
+        }
+        if(minValue == INT_MAX)minValue = -1;
+        unmp[s] = minValue;
+        cout<<s<<" "<<minValue<<endl;
+        return minValue;
+    }
+};
+/*
+
+
+
+5 101
+10 0101
+15 1111
+20 00101
+25 10011
+30 01111
+35 110001
+40 000101
+*/

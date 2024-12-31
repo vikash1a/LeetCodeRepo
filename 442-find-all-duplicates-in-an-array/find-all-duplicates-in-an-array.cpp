063ab4a98a1ac06cef2814f8b958f5758606eed7
Vikash Sinha <vikash1a2b3c@gmail.com>
2024-12-31 14:09:22 +0530

    Time: 0 ms (100.00%) | Memory: 37.7 MB (55.02%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -1,27 +1,41 @@
 class Solution {
 public:
+    // copied 
     vector<int> findDuplicates(vector<int>& nums) {
-        vector<int> duplicates;
-        unordered_set<int> duplicatesSet;
-        int nn = nums.size()-1;
-        for(int i=0;i<=nn;i++){
-            int j = i;
-            while(nums[j]!=(j+1)){
-                if(nums[j] == nums[nums[j]-1]){
-                    
-                    duplicatesSet.insert(nums[j]);
-                    break;
-                }
-                swap(nums[j],nums[nums[j]-1]);
-                // j = nums[j]-1;
+        vector<int>ans;
+        int n=size(nums);
+        for(int i=0;i<n;i++){
+            int x=abs(nums[i]);
+            if(nums[x-1]<0){
+                ans.push_back(x);
             }
+            nums[x-1]*=-1;
         }
-        for(auto it: duplicatesSet){
-            duplicates.push_back(it);
-        }
-
-        return duplicates;
+        return ans;
     }
+    // self
+    // vector<int> findDuplicates(vector<int>& nums) {
+    //     vector<int> duplicates;
+    //     unordered_set<int> duplicatesSet;
+    //     int nn = nums.size()-1;
+    //     for(int i=0;i<=nn;i++){
+    //         int j = i;
+    //         while(nums[j]!=(j+1)){
+    //             if(nums[j] == nums[nums[j]-1]){
+                    
+    //                 duplicatesSet.insert(nums[j]);
+    //                 break;
+    //             }
+    //             swap(nums[j],nums[nums[j]-1]);
+    //             // j = nums[j]-1;
+    //         }
+    //     }
+    //     for(auto it: duplicatesSet){
+    //         duplicates.push_back(it);
+    //     }
+
+    //     return duplicates;
+    // }
 };
 /*
 4,3,2,7,8,2,3,1

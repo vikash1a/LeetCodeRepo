df9453498c0ac41dc5e52bfc7cd5b7b3e2063c48
Vikash Sinha <vikash1a2b3c@gmail.com>
2025-01-25 17:42:32 +0530

    Time: 73 ms (68.01%) | Memory: 87.2 MB (98.20%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
         int nn = nums.size()-1;
-        vector<pair<int,int>> vp(nn+1);
         int totalSum =0;
         for(int i=0;i<=nn;i++){
-            vp[i] = {nums[i],i};
             totalSum+=nums[i];
         }
-        // sort(nums.begin(), nums.end());
         vector<int> sumDiff(nn+1);
         
         int leftSum = 0, rightSum = totalSum;
         for(int i=0;i<=nn;i++){
-            if(i-1>=0)leftSum+=vp[i-1].first;
-            rightSum-=vp[i].first;
-            sumDiff[vp[i].second] = (long)rightSum - (long)((nn-2*i)*vp[i].first)- (long)leftSum;
+            if(i-1>=0)leftSum+=nums[i-1];
+            rightSum-=nums[i];
+            sumDiff[i] = (long)rightSum - (long)((nn-2*i)*nums[i])- (long)leftSum;
         }
         return sumDiff;
     }

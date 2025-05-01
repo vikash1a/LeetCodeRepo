0e004e1a0f18bb3d46c864596821d5e20a2c8c78
Vikash Sinha <vikash1a2b3c@gmail.com>
2025-05-01 23:17:07 +0530

    Time: 679 ms (6.21%) | Memory: 259.9 MB (6.29%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    void process(vector<vector<int>>& result,  priority_queue< vector<int>, vector<vector<int>>, greater<vector<int>> >& pq, int v, int r, int c){
+    void process(vector<vector<int>>& result,  queue< vector<int>>& qu,  int v, int r, int c){
         if(r>=0 && r<=result.size()-1 && c>=0 && c<=result[0].size()-1){
             if(result[r][c]==-1){
                 // cout<<r<<" "<<c<<" "<<v<<endl;
                 result[r][c] = v+1;
-                pq.push({v+1,r,c});
+                qu.push({v+1,r,c});
             }
         }
         
@@ -14,23 +14,23 @@ public:
         int m = isWater.size()-1;
         int n = isWater[0].size()-1;
         vector<vector<int>> result = vector<vector<int>>(m+1, vector<int>(n+1, -1));
-        priority_queue< vector<int>, vector<vector<int>>, greater<vector<int>> > pq;
+        queue< vector<int> > qu;
         for(int i=0;i<=m;i++){
             for(int j=0; j<=n; j++){
                 if(isWater[i][j]==1){
                     result[i][j] = 0;
-                    pq.push({0,i,j});
+                    qu.push({0,i,j});
                 }
             }
         }
-        while(!pq.empty()){
-            vector<int> top = pq.top();
-            pq.pop();
-            int r = top[1], c = top[2], v= top[0];
-            process(result, pq,v, r-1, c);
-            process(result, pq,v, r+1, c);
-            process(result, pq,v, r, c-1);
-            process(result, pq,v, r, c+1);
+        while(!qu.empty()){
+            vector<int> front = qu.front();
+            qu.pop();
+            int r = front[1], c = front[2], v= front[0];
+            process(result, qu,v, r-1, c);
+            process(result, qu,v, r+1, c);
+            process(result, qu,v, r, c-1);
+            process(result, qu,v, r, c+1);
         }
         return result;
     }

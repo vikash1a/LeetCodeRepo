e273b6a91622ae137bb0d05482aa5eab7766aa7b
Vikash Sinha <vikash1a2b3c@gmail.com>
2025-06-08 00:27:30 +0530

    Time: 895 ms (5.01%) | Memory: 13.2 MB (67.47%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> v(n+1, INT_MAX);
+        for(int i=1;i<=sqrt(n);i++){
+            v[i*i] = 1;
+        }
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=i/2;j++){
+                v[i] = min(v[i],v[j]+v[i-j]);
+            }
+        }
+        return v[n];
+    }
+};

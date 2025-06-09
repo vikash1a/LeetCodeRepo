f0d07a86ca5e18bcd123b2017a304166353df737
Vikash Sinha <vikash1a2b3c@gmail.com>
2025-06-09 18:40:10 +0530

    Time: 187 ms (13.87%) | Memory: 131 MB (44.19%) - LeetSync

---
+++ 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxSubArrayLen(vector<int>& nums, int k) {
+        int n = nums.size()-1;
+        unordered_map<long,int> unmp;
+        int maxLen = 0;
+        int sum = 0;
+        unmp[0] = -1;
+        for(int l = 0;l<=n;l++){
+            sum+=nums[l];
+            if(unmp.find(sum)==unmp.end())unmp[sum] = l;
+            long diff = (long)sum-(long)k;
+            if(unmp.find(diff)!=unmp.end()){
+                maxLen = max(maxLen, abs(l-unmp[diff]));
+            }
+            // cout<<"sum - "<<sum<<" maxLen - "<<maxLen<<endl;
+        }
+        return maxLen;
+    }
+};
+/*
+BF - O(N^2)
+
+*/

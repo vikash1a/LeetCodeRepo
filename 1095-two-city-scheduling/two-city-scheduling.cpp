3f2c343f04e813aa2bbd95d5fb51b2b707ebc1cb
Vikash Sinha <vikash1a2b3c@gmail.com>
2025-06-27 18:57:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (46.29%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.cpp
+++ 1095-two-city-scheduling/two-city-scheduling.cpp
@@ -1,36 +1,51 @@
 class Solution {
 public:
-    long util(vector<vector<int>>& costs, int startIndex, int firstCityCount,
-    vector<vector<long>>& dp){
-        int n = costs.size()/2;
-        int secondCityCount = startIndex-firstCityCount;
+    // long util(vector<vector<int>>& costs, int startIndex, int firstCityCount,
+    // vector<vector<long>>& dp){
+    //     int n = costs.size()/2;
+    //     int secondCityCount = startIndex-firstCityCount;
 
-        // cout<<startIndex<<" "<<firstCityCount<<" "<<secondCityCount<<endl;
+    //     // cout<<startIndex<<" "<<firstCityCount<<" "<<secondCityCount<<endl;
 
-        if(firstCityCount==n && secondCityCount==n)return 0;
-        if(startIndex > 2*n-1)return INT_MAX;
-        if(abs(firstCityCount-secondCityCount)> n) return INT_MAX;
+    //     if(firstCityCount==n && secondCityCount==n)return 0;
+    //     if(startIndex > 2*n-1)return INT_MAX;
+    //     if(abs(firstCityCount-secondCityCount)> n) return INT_MAX;
 
-        if(dp[startIndex][firstCityCount]!=-1)return dp[startIndex][firstCityCount];
+    //     if(dp[startIndex][firstCityCount]!=-1)return dp[startIndex][firstCityCount];
 
-        long cost1, cost2;
-        // option 1 fly to first city
-        cost1 = (long)(costs[startIndex][0])+
-        (long)util(costs,startIndex+1,firstCityCount+1,dp);
+    //     long cost1, cost2;
+    //     // option 1 fly to first city
+    //     cost1 = (long)(costs[startIndex][0])+
+    //     (long)util(costs,startIndex+1,firstCityCount+1,dp);
 
-        // option2
-        cost2 = (long)costs[startIndex][1]+
-        (long)util(costs,startIndex+1,firstCityCount,dp);
+    //     // option2
+    //     cost2 = (long)costs[startIndex][1]+
+    //     (long)util(costs,startIndex+1,firstCityCount,dp);
         
-        // cout<<startIndex<<" "<<firstCityCount<<" "<<secondCityCount<<" "<<cost1<<" "<<cost2<<endl;
-        long cost =  min(cost1, cost2);
-        dp[startIndex][firstCityCount] =  cost;
-        return cost;
-    }
+    //     // cout<<startIndex<<" "<<firstCityCount<<" "<<secondCityCount<<" "<<cost1<<" "<<cost2<<endl;
+    //     long cost =  min(cost1, cost2);
+    //     dp[startIndex][firstCityCount] =  cost;
+    //     return cost;
+    // }
+    // int twoCitySchedCost(vector<vector<int>>& costs) {
+    //     int n = costs.size();
+    //     vector<vector<long>> dp = vector<vector<long>>(n+1, vector<long>(n+1, -1));
+    //     return (int)util(costs, 0, 0,dp);
+    // }
+    // greedy 
     int twoCitySchedCost(vector<vector<int>>& costs) {
         int n = costs.size();
-        vector<vector<long>> dp = vector<vector<long>>(n+1, vector<long>(n+1, -1));
-        return (int)util(costs, 0, 0,dp);
+        vector<vector<int>> costDiff;
+        for(int i=0;i<=n-1;i++){
+            costDiff.push_back({costs[i][0]-costs[i][1], i});
+        }
+        sort(costDiff.begin(), costDiff.end());
+        int totalCost = 0;
+        for(int i=0;i<=n-1;i++){
+            if(i<=n/2-1)totalCost+=costs[costDiff[i][1]][0];
+            else totalCost+=costs[costDiff[i][1]][1];
+        }
+        return totalCost;
     }
 };
 /*
